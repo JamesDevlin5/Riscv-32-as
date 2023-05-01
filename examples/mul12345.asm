028d062bf5de310f3cff03dfcbce1dd916cdedef
James Devlin <devlin.ja@northeastern.edu>
2023-04-30 21:14:58 -0400

    Added Octal Number Format

--- examples/mul12345.asm
+++ examples/mul12345.asm
@@ -7,21 +7,21 @@ first:
         li      a5,1
         sw      a5,-20(s0)
 second:
-        # lw      a5,-20(s0)
+        lw      a5,-20(s0)
         slli    a5,a5,1
         sw      a5,-20(s0)
 third:
-        # lw      a4,-20(s0)
+        lw      a4,-20(s0)
         mv      a5,a4
         slli    a5,a5,1
         add     a5,a5,a4
         sw      a5,-20(s0)
 fourth:
-        # lw      a5,-20(s0)
+        lw      a5,-20(s0)
         slli    a5,a5,2
         sw      a5,-20(s0)
 fifth:
-        # lw      a4,-20(s0)
+        lw      a4,-20(s0)
         mv      a5,a4
         slli    a5,a5,2
         add     a5,a5,a4
@@ -29,6 +29,6 @@ fifth:
 done:
         li      a5,0
         mv      a0,a5
-        # lw      s0,28(sp)
+        lw      s0,28(sp)
         addi    sp,sp,32
         jr      ra
